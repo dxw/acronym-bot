ba04cc0bdf80fd9bd673738729dd935e1df0b660
Maria Herrera <maria@dxw.com>
2020-06-18 15:19:29 +0100

    Bot sends messages in-channel
    Now when a user requests the expansion of an acronym, the Acronym bot will reply
    in the channel rather than sending an ephemeral response back to the user.
    To keep the requests anonymous, only the bot response will be display in the
    channel, omiting the slash command that triggered that response.

--- lib/acronym_bot.rb
+++ lib/acronym_bot.rb
@@ -1,13 +1,29 @@
 require 'sinatra'
 require 'yaml'
+require 'json'
+require 'net/http'
+
+# expansion = YAML.load(File.read("./config/acronyms.yml"))
+
+# post '/' do
+#   acronym = params['text']
+#   if expansion.has_key?(acronym)
+#     "#{acronym} stands for #{expansion[acronym]}"
+#   else
+#     "Sorry, I don't know that acronym"
+#   end
+# end
 
 expansion = YAML.load(File.read("./config/acronyms.yml"))
 
 post '/' do
   acronym = params['text']
-  if expansion.has_key?(acronym)
-    "#{acronym} stands for #{expansion[acronym]}"
-  else
-    "Sorry, I don't know that acronym"
-  end
+  url = params['response_url']
+  message = expansion.has_key?(acronym) ? "#{acronym} stands for #{expansion[acronym]}" : "Sorry, I don't know what '#{acronym}' means"
+
+  Net::HTTP.post URI(url),
+       { text: message, response_type: "in_channel" }.to_json,
+       "Content-Type" => "application/json"
+
+  ""
 end
